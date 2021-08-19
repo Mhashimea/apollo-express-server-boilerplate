aa4fe5106070f818fbd0613f394fae782694e9d6
Mhashimea <hashimea12@gmail.com>
2021-08-19 12:57:06 +0530

    added env

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ const { typeDefs } = require("./schema/TypeDefs")
 const { resolvers } = require("./schema/Resolver")
 const { ApolloServer } = require('apollo-server-express')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
 
 let apolloServer = null
 
 async function startApolloServer() {
+  const username = process.env.USER
+  const password = process.env.PASSWORD
   apolloServer = new ApolloServer({ typeDefs, resolvers })
   await apolloServer.start()
   apolloServer.applyMiddleware({ app })
 
-  await mongoose.connect("mongodb+srv://Hashim:hashim123@cluster0.ild9a.mongodb.net/apollo-express-server?retryWrites=true&w=majority", {
+  console.log(username, password)
+
+  await mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.ild9a.mongodb.net/apollo-express-server?retryWrites=true&w=majority`, {
     useUnifiedTopology: true,
     useNewUrlParser: true
   }, (err) => {
